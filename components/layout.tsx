94ffd6aa90e6a4b2a47b2745c6b296bfa01eff06
cjmcintyre <coreyjmcintyre7@gmail.com>
2021-10-13 20:47:53 +1100

    GDPR compliant cookie

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,94 +6,98 @@ import layoutData from "../content/global/index.json";
 import { Theme } from "./theme";
 
 declare global {
-  interface Window { cookieconsent: any; }
+  interface Window { cookieconsent?: any; }
 }
 
 export const Layout = ({ rawData = "", data = layoutData, children }) => {
+  
 
    useEffect(() => {
-            const script = document.createElement("script");
-            script.src = "https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"
-            script.async = false
-            document.body.appendChild(script);
-            window.cookieconsent?.initialise({
-              "palette": {
-                "popup": {
-                  "background": "#6c63ff",
+              window.cookieconsent?.initialise({
+                "palette": {
+                  "popup": {
+                    "background": "#6c63ff",
+                  },
+                  "button": {
+                    "background": "#fff",
+                    "text": "#237afc"
+                  }
                 },
-                "button": {
-                  "background": "#fff",
-                  "text": "#237afc"
-                }
-              },
-              "theme": "classic",
-              "position": "bottom",
-              "type": "opt-out",
-              "content": {
-                "href": "http://localhost:3000/privacy"
-              },
-              "revokable" : true,
-              revokeBtn: `<div class="cc-revoke cc-bottom cc-animate cc-color-override-782958852">🍪 Cookie policy</div>`,
-              onInitialise: function(status) {
-                var type = this.options.type;
-                var didConsent = this.hasConsented();
+                "theme": "classic",
+                "position": "bottom",
+                "type": "opt-in",
+                "content": {
+                  "href": "http://localhost:3000/privacy"
+                },
+                "revokable" : true,
+                revokeBtn: `<div class="cc-revoke cc-bottom cc-animate cc-color-override-782958852">🍪 Cookie policy</div>`,
+                 onInitialise: function(status) {
+                  var type = this.options.type;
+                  var didConsent = this.hasConsented();
 
-                if (type == 'opt-in' && didConsent) {
-                // enable cookies
-                }
-                if (type == 'opt-out' && !didConsent) {
-                // disable cookies
-                }
-              },
-              onStatusChange: function(status, chosenBefore) {
-                var type = this.options.type;
-                var didConsent = this.hasConsented();
-                if (type == 'opt-in' && didConsent) {
-                // enable cookies
-                }
-                if (type == 'opt-out' && !didConsent) {
-                // disable cookies
-                }
-                },
-              onRevokeChoice: function() {
-              var type = this.options.type;
-              
-              if (type == 'opt-in') {
+                  if (type == 'opt-in' && didConsent) {
+                    setCookies()
+                  // enable cookies
+                  }
+                  if (type == 'opt-out' && !didConsent) {
+                    deleteCookies(this.options.cookie.name);
                   // disable cookies
-              }
-              if (type == 'opt-out') {
-                // enable cookies
-
-              }
-              },
-              /* "law": {
-                "regionalLaw": true,
-              },
-              "location": true, */
-              "dismissOnScroll": 200,
-              "dismissOnTimeout": 15000,
-              "dismissOnWindowClick": true,
-            });
-
-            return () => {
-              document.body.removeChild(script);
-            }
+                  }
+                }, 
+                onStatusChange: function(status) {
+                this.hasConsented() ? setCookies() : deleteCookies(this.options.cookie.name);
+                },
+                onRevokeChoice: function() {
+                  var type = this.options.type;
+                  type == "opt-in" ? deleteCookies(this.options.cookie.name) : setCookies();
+                },
+                law: {
+                regionalLaw: false,
+                },
+                "dismissOnScroll": 200,
+                "dismissOnTimeout": 15000,
+                "dismissOnWindowClick": true,
+              });
+            
   }, []);
 
+  function setCookies() {
+      console.log("setCookies hit")
+      var s = document.createElement('script');
+      s.type = "text/javascript"
+      s.async = true;
+      s.src = "https://www.googletagmanager.com/gtag/js?id=G-DC3ZXPZSXL";
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+
+      var script = document.createElement('script');
+      script.innerHTML = `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-DC3ZXPZSXL');`
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(script, x);
+
+      // you can add facebook-pixel and other cookies here
+  };
+
+  function deleteCookies(cookieconsent_name) {
+    console.log(cookieconsent_name, " inside deletecookies");
+    var keep = [cookieconsent_name, "DYNSRV"];
+    
+    document.cookie.split(';').forEach(function(c) {
+        console.log(c);
+        c = c.split('=')[0].trim();
+        if (!~keep.indexOf(c))
+            document.cookie = c + '=;' + 'expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/';
+    });
+  };
   return (
     <>
       <Head>
         <title>Tina</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DC3ZXPZSXL"></script>
-        <script>{
-          `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-DC3ZXPZSXL');`}
-        </script>
+        
         {data.theme.font === "nunito" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -144,6 +148,7 @@ export const Layout = ({ rawData = "", data = layoutData, children }) => {
             />
           </>
         )}
+          <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"></script>
           <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
       </Head>
       <Theme data={data?.theme}>
